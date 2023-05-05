238644715ce716e1f8232dcff9c3df3bf0024c6f
RAHOUANI-Abdessalam <Abdelsalamrahouani@gmail.com>
2023-05-05 22:13:55 +0100

    some ameliorations and a new model

--- chatbot.js
+++ chatbot.js
@@ -1,5 +1,4 @@
 // URL de l'API du chatbot Rasa
-// var chatbotUrl = 'http://localhost:5005/webhooks/rest/webhook';
 var chatbotUrl = 'http://localhost:5005/webhooks/rest/webhook';
 
 // Fonction pour envoyer un message au chatbot et afficher la réponse
@@ -14,7 +13,7 @@ function sendMessageToChatbot(message) {
       message: message
     })
   })
-  .then(response => response.json())http://localhost:5005/webhooks/rest/webhook
+  .then(response => response.json())
   .then(data => {
     // Ajout de la réponse du chatbot à la liste des messages
     var chatMessages = document.getElementById('chat-messages');
@@ -28,9 +27,24 @@ function sendMessageToChatbot(message) {
 // Récupération du champ de saisie de texte et du bouton d'envoi
 var chatInput = document.getElementById('chat-input');
 var chatSendButton = document.getElementById('chat-send-button');
+var chatMessages1 = document.getElementById('chat-messages');
+chatMessages1.innerHTML += '<p><strong>DrBot :</strong> Hi, I am Drbot </p>';
 
 // Ajout d'un gestionnaire d'événement pour le bouton d'envoi
 chatSendButton.addEventListener('click', function() {
+  sendMessage();
+});
+
+// Ajout d'un gestionnaire d'événement pour la touche Entrée
+chatInput.addEventListener('keydown', function(event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    sendMessage();
+  }
+});
+
+// Fonction pour envoyer un message
+function sendMessage() {
   // Récupération du message saisi par l'utilisateur
   var message = chatInput.value;
 
@@ -43,4 +57,5 @@ chatSendButton.addEventListener('click', function() {
 
   // Envoi du message au chatbot
   sendMessageToChatbot(message);
-});
+}
+
