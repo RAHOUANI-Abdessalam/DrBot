ee1dc8ea0ca17607a94c34da189fe2e90b40f632
RAHOUANI-Abdessalam <Abdelsalamrahouani@gmail.com>
2023-07-25 20:58:53 +0100

    final commit .

--- drbot_env/main.py
+++ drbot_env/main.py
@@ -2,6 +2,9 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import subprocess
+import time
+import webbrowser
 
 
 def print_hi(name):
@@ -12,5 +15,22 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    # Start Rasa API server
+    api_command = "rasa run --enable-api --cors \"*\""
+    api_process = subprocess.Popen(api_command.split(), stdout=subprocess.PIPE)
+
+    # Start Rasa action server
+    actions_command = "rasa run actions"
+    actions_process = subprocess.Popen(actions_command.split(), stdout=subprocess.PIPE)
+
+    time.sleep(10)
+
+    # Open index.html in a web browser
+    index_file_path = "index.html"
+    webbrowser.open_new_tab(index_file_path)
+
+    # Wait for the processes to complete
+    api_process.wait()
+    actions_process.wait()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
